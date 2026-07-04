0a394b8bcc3a95e02be9e673b0a58e7bac2491a5
Ngan Huynh <nhuynh1922@gmail.com>
2026-07-03 22:16:48 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,2 +1,26 @@
-def test_simple():
-    assert 1 + 1 == 2
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_prediction():
+    sample = {
+        "Pclass": 3,
+        "Age": 30,
+        "SibSp": 1,
+        "Parch": 0,
+        "Fare": 10.5,
+        "Sex_male": 1,
+        "Embarked_Q": 0,
+        "Embarked_S": 1
+    }
+
+    response = client.post("/predict", json=sample)
+
+    assert response.status_code == 200
+    assert "prediction" in response.json()
+    assert "risk_probability" in response.json()
